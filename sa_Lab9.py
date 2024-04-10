3570885a1237f33f5a7f2d6db235dbb1621ba5d1
5anti <80968125+5anti@users.noreply.github.com>
2024-04-10 08:40:30 -0400

    Create sa_Lab9.py

---
+++ sa_Lab9.py
@@ -0,0 +1,61 @@
+#
+# Santiago Alvarez
+#
+# April 10, 2024
+#
+
+
+def decode(encoded_passwd):
+    """Decodes password by shifting numbers
+    3 times back"""
+
+    passwd = ''
+
+    for num in encoded_passwd:
+        num = int(num)
+        new_num = (num - 3) % 10
+        passwd += new_num
+
+    return passwd
+
+
+def main():
+    menu = '''
+    Password Encoder/Decoder
+    ------------------------
+    1. Encode password
+    2. Decode password
+    3. Print menu
+    4. Exit
+    '''
+    print(menu)
+
+    while True:
+        option = str(input('\n>> What would you like to do? '))
+
+        # Option 1: Encode password
+        if option == '1':
+            '''passwd = str(input('> Input password to encode: '))
+            result = encode(passwd)
+            print('[+] Encoded password: {}\n'.format(result))'''
+
+            pass
+
+        # Option 2: Decode password
+        elif option == '2':
+            passwd = str(input('> Input password to decode: '))
+            result = decode(passwd)
+            print('[+] Decoded password: {}'.format(result))
+
+        elif option == '3':
+            print(menu)
+
+        elif option == '4':
+            break
+
+        else:
+            print('[!] ERROR: Option not recognized.')
+
+
+if __name__ == '__main__':
+    main()
